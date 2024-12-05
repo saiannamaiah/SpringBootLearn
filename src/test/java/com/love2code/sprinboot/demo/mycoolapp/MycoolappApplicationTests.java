7f60cae9a38e95d0e8229d3dcafd2db32768284d
SaiAnnamaiahB <annamaiahsai@gmail.com>
2024-12-05 17:45:24 +0530

    Initial Commit of Learning Springboot POM file and Application properties file

---
+++ src/test/java/com/love2code/sprinboot/demo/mycoolapp/MycoolappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.love2code.sprinboot.demo.mycoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
